5997074ee176dc0cbd29dc77b36385d0edd27588
Rajesh <rgulivindala1146@conestoga.on.ca>
2025-03-26 14:30:33 -0400

    test: handle edge case of zero dimensions

--- src/calculateCanvasPerimeter/calculateCanvasSize.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasSize.test.ts
@@ -5,5 +5,8 @@ describe("calculateCanvasSize", () => {
     expect(calculateCanvasSize("10", "100")).toEqual(220);
     expect(calculateCanvasSize("5", "7")).toEqual(24);
   });
-  
+  test("handles zero values", () => {
+    expect(calculateCanvasSize("0", "10")).toEqual(20);
+    expect(calculateCanvasSize("0", "0")).toEqual(0);
+  });
 });
