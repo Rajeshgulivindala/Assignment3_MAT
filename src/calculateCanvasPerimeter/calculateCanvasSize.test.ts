413ad2f50ff198696d8d694280ddc5993d468ecc
Rajesh <rgulivindala1146@conestoga.on.ca>
2025-03-26 14:29:34 -0400

    test: add cases for normal length and width inputs

--- src/calculateCanvasPerimeter/calculateCanvasSize.test.ts
+++ src/calculateCanvasPerimeter/calculateCanvasSize.test.ts
@@ -1,9 +1,9 @@
 import calculateCanvasSize from "./calculateCanvasSize";
 
 describe("calculateCanvasSize", () => {
-  test("returns correct result", () => {
-    const result = calculateCanvasSize("10", "100");
-
-    expect(result).toEqual(1000);
-  })
+  test("returns correct result for normal inputs", () => {
+    expect(calculateCanvasSize("10", "100")).toEqual(220);
+    expect(calculateCanvasSize("5", "7")).toEqual(24);
+  });
+  
 });
